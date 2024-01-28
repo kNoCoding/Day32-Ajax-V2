19294bae0fb456300863362fceabc63672021010
Konstantin Nochevny <kosnobiz@gmail.com>
2024-01-28 12:42:28 +0200

    Style the contacts

--- js/address-book.js
+++ js/address-book.js
@@ -30,30 +30,33 @@ function renderContacts(contacts) {
         strHtml += `
                 <div class="single-contact">
                 <p> ${contact.lname}, ${contact.fname} </p>
-                <img src="https://robohash.org/${contact.fname}?set=set5" alt="Image of ${contact.fname}">
 
-                <table>
-                    <tr>
-                        <td>Phone: </td>
-                        <td>${contact.tel}</td>
-                    </tr>
-                    <tr>
-                        <td>City: </td>
-                        <td>${contact.city}</td>
-                    </tr>
-                    <tr>
-                        <td>State: </td>
-                        <td>${contact.state}</td>
-                    </tr>
-                    <tr>
-                        <td>Zip: </td>
-                        <td>${contact.zip}</td>
-                    </tr>
-                    <tr>
-                        <td>Address: </td>
-                        <td>${contact.address}</td>
-                    </tr>
-                </table>
+                    <div class="image-and-table-container">
+                        <img src="https://robohash.org/${contact.fname}?set=set5" alt="Image of ${contact.fname}">
+
+                        <table>
+                            <tr>
+                                <td>Phone: </td>
+                                <td>${contact.tel}</td>
+                            </tr>
+                            <tr>
+                                <td>City: </td>
+                                <td>${contact.city}</td>
+                            </tr>
+                            <tr>
+                                <td>State: </td>
+                                <td>${contact.state}</td>
+                            </tr>
+                            <tr>
+                                <td>Zip: </td>
+                                <td>${contact.zip}</td>
+                            </tr>
+                            <tr>
+                                <td>Address: </td>
+                                <td>${contact.address}</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
                 `
     })
