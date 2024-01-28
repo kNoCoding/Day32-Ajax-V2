a24ef3bb47db83e9c18621e01da0503f1ced0f3f
Konstantin Nochevny <kosnobiz@gmail.com>
2024-01-28 13:26:31 +0200

    Adhere to MVC

--- js/address-book.js
+++ js/address-book.js
@@ -1,23 +1,7 @@
 
-const url = `http://www.filltext.com/?rows=6&fname=%7bfirstName%7d&lname=%7blastName%7d&tel=%7bphone|format%7d&address=%7bstreetAddress%7d&city=%7bcity%7d&state=%7busState|abbr%7d&zip=%7bzip%7d&pretty=true`
-
 
 function onInit() {
     getAns(renderContacts)
-}
-
-function getAns(callback) {
-    const xhr = new XMLHttpRequest()
-
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            const ans = JSON.parse(xhr.responseText)
-
-            callback(ans)
-        }
-    }
-    xhr.open('GET', url, true)
-    xhr.send()
 }
 
 function renderContacts(contacts) {
